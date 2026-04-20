435b5b2dc249144bcb180493421090a868a8e3b8
david <davidbisht>
2026-04-20 10:54:06 +0530

    Initial Commit

---
+++ clothingclick/clothingclickApp.swift
@@ -0,0 +1,17 @@
+//
+//  clothingclickApp.swift
+//  clothingclick
+//
+//  Created by DavidBisht on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct clothingclickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
